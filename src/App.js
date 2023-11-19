cd4845e98a15bda13c02a57cd1b1da5c7c6ea726
Michael Descorcier <mdescorcier-sti@avista.fr>
2023-11-19 18:09:04 +0100

    testOrientation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
-
 import Home from './pages/Home';
-import SitePlan from'./pages/SitePlan';
-import LegalMention from'./pages/LegalMention';
-import ConfidentialPolitics from'./pages/ConfidentialPolitics';
+import SitePlan from './pages/SitePlan';
+import LegalMention from './pages/LegalMention';
+import ConfidentialPolitics from './pages/ConfidentialPolitics';
 
 function App() {
+  useEffect(() => {
+    const handleOrientationChange = () => {
+      // Verrouiller l'orientation en "portrait-primary"
+      if (window.screen.orientation) {
+        window.screen.orientation.lock('portrait-primary').catch((error) => {
+          console.error('Erreur lors du verrouillage de l\'orientation :', error);
+        });
+      }
+    };
+
+    // Ajouter un écouteur d'événements pour le changement d'orientation
+    window.addEventListener('orientationchange', handleOrientationChange);
+
+    // Configuration initiale
+    handleOrientationChange();
+
+    // Nettoyage lors du démontage du composant
+    return () => {
+      window.removeEventListener('orientationchange', handleOrientationChange);
+    };
+  }, []);
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path = "/" element = {<Home />} />
-          <Route path = "/Plan-du-site" element = {<SitePlan />} />
-          <Route path = "/Mentions-légales" element = {<LegalMention />} />
-          <Route path = "/Politique-de-confidentialité" element = {<ConfidentialPolitics />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Plan-du-site" element={<SitePlan />} />
+          <Route path="/Mentions-légales" element={<LegalMention />} />
+          <Route path="/Politique-de-confidentialité" element={<ConfidentialPolitics />} />
         </Routes>
       </BrowserRouter>
     </>
@@ -22,5 +44,3 @@ function App() {
 }
 
 export default App;
-
-
